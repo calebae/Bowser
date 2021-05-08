50508c2a8f6d004730d1859e8364b6822e2cd19f
calebae <ecbae91@gmail.com>
2021-05-08 10:06:14 -0400

    Attempt
    For external package

--- R/getSimData.R
+++ R/getSimData.R
@@ -3,7 +3,8 @@
 #'
 #' @param mask.img Maksed image
 #' @param background.img Image to be used to generate the normal state
-#' @param location Location of clusters to be simulated
+#' @param x_location Location in X axis of clusters to be simulated
+#' @param y_location Location in Y axis of clusters to be simulated
 #' @param K Number of clusters to be simulated
 #' @param size Size of clusters to be simulated
 #' @param int How high the cluster value should be
@@ -13,7 +14,7 @@
 #'
 
 
-getSimData <- function(mask.img, background.img, location, K, size, int, set.seed = 1){
+getSimData <- function(mask.img, background.img, x_location, y_location, K, sizes, ints, set.seed = 1){
 
   # Mask
   mask <- antsImageRead(mask.img)
@@ -47,12 +48,17 @@ getSimData <- function(mask.img, background.img, location, K, size, int, set.see
                    c = as.vector(col(slice)),
                    slice = as.vector(slice))
 
+  if(K==1){
+    stopifnot(length(x_location)==1)
+    stopifnot(length(y_location)==1)
+    stopifnot(length(sizes)==1)
+    stopifnot(length(ints)==1)
 
   # Simulation settings
-  loc1s <- location[1]
-  loc2s <- location[2]
-  sizes <- c(2, 4, 6, 3, 5)
-  ints <- c(100, 200, 400, 300)
+  loc1s <- x_location
+  loc2s <- y_location
+  size <- sizes
+  int <- ints
   # num <- c(1, 2, 3)
 
   # Add background
@@ -70,5 +76,45 @@ getSimData <- function(mask.img, background.img, location, K, size, int, set.see
   img <- array(img, dim = c(64,64))
 
   return(img)
+  }else{
+    stopifnot(length(x_location)==K)
+    stopifnot(length(y_location)==K)
+    stopifnot(length(sizes)==K)
+    stopifnot(length(ints)==K)
+    # Array to be returned
+    image.array <- array(0, dim = c (64,64,K))
+
+    # Simulation settings
+    loc1s <- x_location
+    loc2s <- y_location
+
+    # Loop through all combos:
+    set.seed(032720)
+    for(i in 1:K){
+      loc1 <- loc1s[i]
+      loc2 <- loc2s[i]
+      for(size in sizes){
+        for(int in ints){
+
+            # Add background
+            df$background <- 0
+            df$background[slice == 1] <- sample(meanslice, size = 1867, replace = T)
+
+            # Add disease spot
+            df$dist <- rdist::cdist(df[,c('r','c')], cbind.data.frame(loc1, loc2))
+            df$spot <- 0
+            df$spot[df$dist <= size] <- int
+            df$img <- df$background + df$spot
+
+            # Convert back to matrix format
+            img <- df$img
+            img <- array(img, dim = c(64,64))
+            # lattice::levelplot(img)
+            image.array[,,K] <- img
+        }
+      }
+    }
+    return(image.array)
+  }
 }
 
