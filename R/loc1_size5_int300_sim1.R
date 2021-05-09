723ab0e81de8261daa79ea1a799520ded7cb4142
calebae <ecbae91@gmail.com>
2021-05-09 12:01:52 -0400

    Remove redundant data
    remove unused data

--- R/loc1_size5_int300_sim1.R
+++
@@ -1,9 +0,0 @@
-#' Simulation data with disease located at (26,16)
-#'
-#' A simulated dataset with disease located at (26,16), with size 5 and intensity +300 from background.
-#'
-#' @format A matrix with 64 rows and 64 columns.
-#'
-#' @source \url{https://github.com/ryansar/lungct}
-#'
-"loc1_size5_int300_sim1"
--- R/loc4_size3_int300_sim1.R
+++
@@ -1,9 +0,0 @@
-#' Simulation data with disease located at (40,21)
-#'
-#' A simulated dataset with disease located at (40,21), with size 3 and intensity +300 from background.
-#'
-#' @format A matrix with 64 rows and 64 columns.
-#'
-#' @source \url{https://github.com/ryansar/lungct}
-#'
-"loc4_size3_int300_sim1"
